3282d02abaea658a600644e2d25c7ee45d993dba
Yichun Yin <ssggjjdd@163.com>
2018-04-23 19:21:21 +0800

    minor changes

--- lm/ptb_word_lm.py
+++ lm/ptb_word_lm.py
@@ -428,6 +428,7 @@ def main(_):
   raw_data = reader.ptb_raw_data(FLAGS.data_path)
   train_data, valid_data, test_data, _ = raw_data
 
+  print('dataload finished!')  
   config = get_config()
   eval_config = get_config()
   eval_config.batch_size = 1
