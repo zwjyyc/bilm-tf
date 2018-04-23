eaf066a9d7d1508a9e31043d327be5ec72f54d47
Yichun Yin <ssggjjdd@163.com>
2018-04-23 16:00:17 +0800

    print dict

--- lm/output_dict.py
+++ lm/output_dict.py
@@ -8,6 +8,6 @@ with open(sys.argv[1], 'r') as fin:
     vocab = pickle.load(fin)
     token2ids = vocab.token2id
 
-    for token, id in enumerate(token2ids.items()):
+    for token, id in token2ids.items():
         out_str = '{}\t{}'.format(token, id)
         print(out_str.encode('utf-8'))
