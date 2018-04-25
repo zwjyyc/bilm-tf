036a7d7b0d89051a56e95b790acd3999c4886ad1
Yichun Yin <ssggjjdd@163.com>
2018-04-25 22:08:40 +0800

    minor changes

--- lm/ptb_word_lm.py
+++ lm/ptb_word_lm.py
@@ -509,6 +509,8 @@ def _pretrained_initializer(varname, weight_file):
     We'll stub out all the initializers in the pretrained LM with
     a function that loads the weights from the file
     '''
+    if varname == 'embedding':
+      varname = 'Model/embedding:0'
     with h5py.File(weight_file, 'r') as fin:
         weights = fin[varname]
 
